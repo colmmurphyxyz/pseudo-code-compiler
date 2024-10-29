77625b9aa2039d9fdb9693932791612d680a349c
Colm Murphy <colmmurphy016@gmail.com>
2024-10-29 09:28:25 +0000

    ignore .pyc files

--- src/parser.py
+++ src/parser.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     print("opening", grammar_file_path)
     with open(grammar_file_path, "r") as in_file:
         grammar: str = in_file.read()
-        parser = Lark(grammar, start="file_input", postlex=PythonIndenter(), debug=True)
+        parser = Lark(grammar, start="file_input", postlex=PythonIndenter())
     with open(sys.argv[1], "r") as in_file:
         source = in_file.read()
     # source: str = " ".join(sys.argv[1:])
