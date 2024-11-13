8f42471bf87cbe8c3b25f46bd74be848fced9511
Colm Murphy <colmmurphy016@gmail.com>
2024-11-13 12:36:14 +0000

    Create PostLexPipeline class to wrap multiple post-lexers together

---
+++ src/postlex_pipeline.py
@@ -0,0 +1,17 @@
+from functools import reduce
+from typing import Iterator
+
+from lark import Token
+from lark.lark import PostLex
+
+
+class PostLexPipeline(PostLex):
+    def __init__(self, postlexers: list[PostLex]):
+        super().__init__()
+        self.postlexers: list[PostLex] = postlexers
+
+    def process(self, stream: Iterator[Token]) -> Iterator[Token]:
+        output: Iterator[Token] = stream
+        for postlexer in self.postlexers:
+            output = postlexer.process(output)
+        return output
