34a73c178160bd86af1d39e27a12942941df8bb6
Colm Murphy <colmmurphy016@gmail.com>
2024-11-13 12:37:40 +0000

    Create class to represent unicode sub-strings of an identifier

---
+++ src/unicode_fragment.py
@@ -0,0 +1,36 @@
+from unicodeitplus import replace
+
+class UnicodeFragment:
+
+    def __init__(self, source: str):
+        self._ascii: str = source
+
+    @property
+    def ascii(self) -> str:
+        return self._ascii
+
+    @property
+    def transformed(self) -> str:
+        return replace(self._ascii)
+
+    def __str__(self):
+        return f"UnicodeFragment(\"{self._ascii}\")"
+
+def split_unicode_fragments(s: str) -> list[str | UnicodeFragment]:
+    split: list[str | UnicodeFragment] = []
+    curr: str = ""
+    is_ascii = True
+    for character in s:
+        if character == "$":
+            # add curr to split
+            # if currently in unicode mode, append as unicode fragment
+            if is_ascii:
+                split.append(curr)
+            else:
+                split.append(UnicodeFragment(curr))
+            curr = ""
+            is_ascii = not is_ascii
+        else:
+            curr += character
+    if curr != "": split.append(curr)
+    return split
