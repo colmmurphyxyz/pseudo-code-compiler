565d0a60177582536eea866c1fe19b8ebf8d22de
Colm Murphy <colmmurphy016@gmail.com>
2024-11-18 16:26:06 +0000

    Fix detokenizer

--- src/print_detokenization.py
+++ src/print_detokenization.py
@@ -9,7 +9,12 @@ class PrintDetokenization(PostLex):
     def __init__(self, output: TextIO=sys.stderr) -> None:
         self._output: TextIO = output
 
+    def iswhitespace(self, token: Token) -> bool:
+        return token.strip() == ""
+
     def process(self, stream: Iterator[Token]) -> Iterator[Token]:
         for token in stream:
-            print(token.value, end="", flush=True, file = self._output)
+            print(token.value, end="", file = self._output)
+            if len(token) > 0 and not self.iswhitespace(token[-1]):
+                print(" ", end="", flush=True, file=self._output)
             yield token
