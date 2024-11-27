bedd8dab86e2085cb10d35c6f24322e2528a8f4f
Colm Murphy <colmmurphy016@gmail.com>
2024-11-27 18:02:36 +0000

    add RANDOM(a, b) stdlib function

--- src/backend/pc_stdlib/__init__.py
+++ src/backend/pc_stdlib/__init__.py
@@ -1,5 +1,7 @@
 from .pc_array import PcArray
+from .pc_random import RANDOM
 
 __all__ = [
-    "PcArray"
+    "PcArray",
+    "RANDOM"
 ]
---
+++ src/backend/pc_stdlib/pc_random.py
@@ -0,0 +1,4 @@
+from random import randint
+
+def RANDOM(a: int, b: int) -> int:
+    return randint(a, b)
