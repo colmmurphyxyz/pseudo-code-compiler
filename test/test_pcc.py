0090d8650ea70f77f786c6277d05675c37ae22bd
Colm Murphy <colmmurphy016@gmail.com>
2025-01-07 14:58:09 +0000

    create test suite

--- test/test_pcc.py
+++ test/test_pcc.py
@@ -1,8 +1,18 @@
 import unittest
 
+from test.frontend.test_unicode_fragment import TestUnicodeFragment
+from test.frontend.test_unicode_formatter import TestUnicodeFormatter
+
 class TestPcc(unittest.TestCase):
     def test_my_test(self):
         self.assertTrue(1 > 0)
 
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestUnicodeFragment("test_unicode_fragment"))
+    suite.addTest(TestUnicodeFormatter("test_unicode_formatter"))
+    return suite
+
 if __name__ == '__main__':
-    unittest.main()
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
