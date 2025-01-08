f73135592e09786a70f2f75bbedc9b543a40305a
Colm Murphy <colmmurphy016@gmail.com>
2025-01-08 11:30:29 +0000

    rename _normalize_identifier

--- pcc/backend/transpiler.py
+++ pcc/backend/transpiler.py
@@ -241,7 +241,7 @@ from pcc.backend.pc_stdlib import *
     def var(self, args) -> str:
         return str(args[0])
 
-    def _normalize_identifier(self, name: str) -> str:
+    def __normalize_identifier(self, name: str) -> str:
         normalized: str = (
             name
             .replace("-", "_")
@@ -256,7 +256,7 @@ from pcc.backend.pc_stdlib import *
         return normalized
 
     def name(self, args: list[Token]) -> str:
-        return self._normalize_identifier(str(args[0].value))
+        return self.__normalize_identifier(str(args[0].value))
 
     def _NEWLINE(self, args) -> str:
         return "\n"
