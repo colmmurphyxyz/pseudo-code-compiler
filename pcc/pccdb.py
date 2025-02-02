7412a2b110ca8a522e4edac69ef32858a306ebcd
Colm Murphy <colmmurphy016@gmail.com>
2025-02-02 15:17:00 +0000

    working on it

--- pcc/pccdb.py
+++ pcc/pccdb.py
@@ -1,43 +1,94 @@
+import inspect
+import os
 from pdb import Pdb
 import sys
 from types import FrameType
-import pathlib
+import re
 
 __all__ = ["Pccdb", "set_trace"]
 
+from typing import TextIO, Any, Mapping
+
 
 class Pccdb(Pdb):
-    __pc_source: list[str]
-    __source_directory: str
+    active_instance = None
+
+    __pc_source_lines: list[str]
+
+    _pdb_output: TextIO
 
     @property
-    def pc_source(self) -> list[str]:
-        return self.__pc_source
-
-    @property
-    def source_directory(self) -> str:
-        return self.__source_directory
+    def pc_source_lines(self) -> list[str]:
+        return self.__pc_source_lines
 
     def __init__(self, pc_source: str):
-        pdb_output = open("temp.txt", "a", encoding="utf8")
-        super().__init__(stdin=sys.stdin, stdout=pdb_output, readrc=False)
-
+        Pccdb.active_instance = self
+        self._pdb_output = open("temp.txt", "w", encoding="utf8")
+        super().__init__(stdin=sys.stdin, stdout=sys.stdout, readrc=False)
         self.prompt = "(Pccdb) "
-        self.__pc_source = pc_source.splitlines()
-        self.__source_directory = str(pathlib.Path(__file__).parent.absolute())
+        self.__pc_source_lines = pc_source.splitlines()
 
+    def __del__(self):
+        self._pdb_output.close()
+        if self is Pccdb.active_instance:
+            Pccdb.active_instance = None
 
-    def set_next(self, frame: FrameType):
-        filename: str = frame.f_globals["__file__"]
-        py_line_number: int = frame.f_lineno
-        with open(filename, 'r') as file:
-            lines: list[str] = file.readlines()
-        print("###", lines[py_line_number].strip())
-        pc_line_number: int = int(lines[py_line_number].split("# l:")[1])
-        print(f"PC Line Number {pc_line_number}")
-        pc_line: str = self.__pc_source.splitlines()[pc_line_number - 1]
-        print("@@@", pc_line)
-        super().set_trace(frame)
+    def get_locals(self) -> Mapping[str, Any]:
+        return self.curframe_locals
+
+    def get_locals_sanitised(self) -> dict[str, any]:
+        local_variables = self.get_locals()
+        hidden_names = { "__name__", "__doc__", "__package__", "__loader__", "__spec__", "__annotations__",
+                         "__builtins__", "__file__", "__cached__", "pathlib", "sys", "set_trace",
+                         "__pdb_convenience_variables"}
+        return {k: v for k, v in local_variables.items() if k not in hidden_names}
+
+    def get_globals(self) -> dict[str, Any]:
+        return self.curframe.f_globals
+
+    def get_globals_sanitised(self) -> dict[str, Any]:
+        hidden_names = {"__name__", "__doc__", "__package__", "__loader__", "__spec__", "__annotations__",
+                        "__builtins__", "__file__", "__cached__", "pathlib", "sys", "set_trace",
+                        "__pdb_convenience_variables"}
+        return {k: v for k, v in self.get_globals().items() if k not in hidden_names}
+
+    def _has_line_marker(self, line: str) -> bool:
+        return re.match(r"# l:\d+", line) is not None
+
+    def do_step(self, arg):
+        print("stepping")
+        frame = self.curframe
+        current_file = frame.f_code.co_filename
+        current_module = inspect.getmodule(frame)
+        super().do_step(arg)
+        new_frame = self.curframe
+        if new_frame:
+            new_file = new_frame.f_code.co_filename
+            new_module = inspect.getmodule(new_frame)
+            if new_file != current_file or new_module != current_module:
+                self.do_until(None)
+
+    def do_next(self, arg):
+        """Execute the next line, stepping over functions."""
+        print("next")
+        frame = self.curframe
+        if frame:
+            # Get the current line's file and module
+            current_file = frame.f_code.co_filename
+            current_module = inspect.getmodule(frame)
+
+            # Execute the next line
+            super().do_next(arg)
+
+            # Check if the new frame is in the same file or module
+            new_frame = self.curframe
+            if new_frame:
+                new_file = new_frame.f_code.co_filename
+                new_module = inspect.getmodule(new_frame)
+
+                # If the new frame is in an external module, step out of it
+                if new_file != current_file or new_module != current_module:
+                    self.do_until(None)  # Step out of the external function
 
     def do_list(self, arg):
         print("FGHJK")
@@ -51,11 +102,36 @@ class Pccdb(Pdb):
     def do_longlist(self, arg):
         self.do_list(arg)
 
-def set_trace(pc_source_file: str, *, header=None):
-    with open(pc_source_file, "r") as file:
-        pc_source: str = file.read()
-    pdb = Pccdb(pc_source)
-    print("Created Pccdb instance", sys._getframe())
+    def do_z(self, arg):
+        """Custom command to step into internal functions and step over external ones."""
+        # Get the current frame and file
+        frame = self.curframe
+        if frame:
+            self.current_file = frame.f_code.co_filename  # Update the current file
+
+        # Execute the next line
+        print("do_next call")
+        self.onecmd("next")
+
+        # Check if we're in an external module
+        new_frame = self.curframe
+        new_file = new_frame.f_code.co_filename
+        if new_frame:
+            if new_file != self.current_file:
+                print("external")
+                # If we're in an external module, step out of it
+                self.onecmd("return")
+            else:
+                print("should step in")
+                # If we're in the current file, step into the function
+                self.onecmd("step")
+        else:
+            print("new frame none")
+
+
+
+def set_trace(pc_source_code: str, *, header=None):
+    pdb = Pccdb(pc_source_code)
     if header is not None:
         pdb.message(header)
     pdb.set_trace(sys._getframe().f_back)
