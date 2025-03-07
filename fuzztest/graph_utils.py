ec6a86e907c9bee04cb2270d020c86493454907b
Colm Murphy <colmmurphy016@gmail.com>
2025-03-07 16:20:20 +0000

    unit tests for graph BFS/DFS

---
+++ fuzztest/graph_utils.py
@@ -0,0 +1,23 @@
+import sys
+import pathlib
+
+from pcc.backend.pc_stdlib import PcVertex, PcGraph, PcArray
+
+# add the parent directory (repository root) to sys.path
+sys.path.append(str(pathlib.Path(__file__).parent.parent.absolute()))
+
+
+def make_graph():
+    adjmat = PcArray.from_py_list([ PcArray.from_py_list([ 0 for _ in range(5)], 1) for _ in range(5) ])
+    adjmat[1][2] = 10
+    adjmat[1][4] = 5
+    adjmat[2][3] = 1
+    adjmat[2][4] = 2
+    adjmat[3][5] = 4
+    adjmat[4][2] = 3
+    adjmat[4][3] = 9
+    adjmat[4][5] = 2
+    adjmat[5][3] = 6
+    adjmat[5][1] = 7
+
+    return PcGraph(adjmat)
---
+++ fuzztest/test_graph_dfs.py
@@ -0,0 +1,32 @@
+import sys
+import pathlib
+import unittest
+
+from pcc.backend.pc_stdlib import PcGraph, PcVertex
+
+# add the parent directory (repository root) to sys.path
+sys.path.append(str(pathlib.Path(__file__).parent.parent.absolute()))
+
+from graph_utils import make_graph
+from res.transpiled_pc_examples.ch20.graph_dfs import DFS
+
+class TestGraphDFS(unittest.TestCase):
+    def test_bfs(self):
+        g = make_graph()
+        DFS(g)
+        vertices: list[PcVertex | None] = [ None for _ in range(6) ]
+        for v in g.V:
+            vertices[v.key] = v
+        # assert all vertices were visited
+        for v in vertices[1:]:
+            self.assertEqual(v.color, "BLACK")
+        self.assertEqual(vertices[1].d, 1)
+        self.assertEqual(vertices[2].pi, vertices[1])
+        self.assertEqual(vertices[2].d, 2)
+        self.assertEqual(vertices[3].pi, vertices[2])
+        self.assertEqual(vertices[3].d, 3)
+        self.assertEqual(vertices[4].pi, vertices[2])
+        self.assertEqual(vertices[4].d, 3)
+        self.assertEqual(vertices[5].pi, vertices[3])
+        self.assertEqual(vertices[5].d, 4)
+
